1d0034e97d45af89192984ab18258041be18ab57
jojolepnj <109385712+jojolepnj@users.noreply.github.com>
2025-02-03 09:58:43 +0100

    fix erreur udp

--- depot/UDP/client_udp_division.c
+++ depot/UDP/client_udp_division.c
@@ -5,25 +5,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-#include <string.h>
-#include <netinet/in.h>
-
-int main(int argc, char** argv) {
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port>\nmauvais nombre d'arguments");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port>\n<adresse IP> est une adresse IP au format décimal pointé");
-	//traiter_commande(/*A COMPLETER*/, argv[0], "<adresse IP> <port>\n<port> est un port non réservé");
-
-	/*A COMPLETER*/
-
-	exit(0);
-=======
 #include <netinet/in.h>
 
 int traiter_commande_wrapper(int argc, char** argv) {
     traiter_commande(argc != 3, argv[0], "<adresse IP> <port>\nmauvais nombre d'arguments");
     traiter_commande(est_une_adresse_IP(argv[1]) == 0, argv[0], "<adresse IP> <port>\n<adresse IP> est une adresse IP au format décimal pointé");
     traiter_commande(atoi(argv[2]) < 1024, argv[0], "<adresse IP> <port>\n<port> est un port non réservé");
+    return 0;
 }
 
 int main(int argc, char** argv) {
@@ -51,5 +39,4 @@ int main(int argc, char** argv) {
     printf("Message reçu -> %s\n", message);
     fermer_connexion(&sock);
     return 0;
->>>>>>> 6f32960 (cr??ation serveur et client udp)
 }
